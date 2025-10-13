92af71ef72a6c7f8532b73e94f8b5d4e34b7b41d
Anas Abdelhakim <anasabdoali22@gmail.com>
2025-10-13 07:32:36 +0300

    first commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewCompanioion = () => {
+  return (
+    <div>NewCompanioion</div>
+  )
+}
+
+export default NewCompanioion
---
+++ app/subscribtion/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Subscribtion = () => {
+  return <div>Subscribtion</div>;
+};
+
+export default Subscribtion;
